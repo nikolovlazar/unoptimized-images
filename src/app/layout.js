61a823dec4645758d70264e0b0a2cc2dfbe7e00a
Lazar Nikolov <hello@nikolovlazar.com>
2024-02-06 07:58:25 -0500

    scaffold

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="flex gap-4 px-4 py-2">
+          <Link href="/">Unoptimized</Link>
+          <Link href="/next-image">next/image</Link>
+          <Link href="/optimized">Optimized</Link>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
